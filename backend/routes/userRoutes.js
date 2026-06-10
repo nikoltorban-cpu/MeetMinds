36091b129efe4af904e7c6c15dd0c3ec7c6cd349
Nikol Torban <nikol.torban@gmail.com>
2026-06-10 21:39:46 +0300

    skkdk

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { users, userId } from "../controllers/usersController"
+import { users, userId } from "../controllers/usersController.js"
 
 router.get("/users", users);
 router.get("/user/:id", userId);
