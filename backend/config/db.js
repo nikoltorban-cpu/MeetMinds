76d5c391e1d04b6d787581f9f9325918f4384ba5
Nikol Torban <nikol.torban@gmail.com>
2026-06-17 12:24:31 +0300

    fixed api url

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,19 +5,11 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const pool = process.env.DATABASE_URL
-  ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    })
-  : new Pool({
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-    });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
